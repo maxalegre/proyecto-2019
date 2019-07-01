a078229213a652b48915b78c8b0ebbbd74557fc9
LeonardoVal <leonardo.val@creatartis.com>
2019-07-01 18:16:23 -0300

    Started testing
    + And hence inevitably corrections have to be done.
    + Operator validation in binary and unary operators was broken.
    + Compilation of object expressions uses streams now.
    + Method `toJS` moved to static.

--- src/main/uy/edu/ucu/jsonql2019/eval/JSONQL_JSCompiler.java
+++ src/main/uy/edu/ucu/jsonql2019/eval/JSONQL_JSCompiler.java
@@ -1,6 +1,7 @@
 package uy.edu.ucu.jsonql2019.eval;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import uy.edu.ucu.jsonql2019.JSONQLVisitor;
 import uy.edu.ucu.jsonql2019.ast.*;
@@ -11,8 +12,10 @@ public class JSONQL_JSCompiler implements JSONQLVisitor<StringBuilder, StringBui
 	
 	/** Compile the given JSONQL `ast` to Javascript.
 	 */
-	public String toJS(JSONQLExpression ast) {
-		return ast.traverse(this, new StringBuilder()).toString();
+	public static String toJS(JSONQLExpression ast) {
+		StringBuilder code = new StringBuilder("(function (){ return ");
+		ast.traverse(new JSONQL_JSCompiler(), code);
+		return code.append("; })").toString();
 	}
 
 	/** Helper method to convert to JavaScript a list of expression, separated by commas.
@@ -38,30 +41,32 @@ public class JSONQL_JSCompiler implements JSONQLVisitor<StringBuilder, StringBui
 	}
 
 	@Override public StringBuilder visit(JSONQLBinaryExpression ast, StringBuilder context) {
-		if ("* / + - == != < >= > >= && || ".indexOf(ast.operator +" ") < 0) { // A JavaScript operator.
-			ast.left.traverse(this, context.append("("));
-			ast.right.traverse(this, context.append(" "));
-			return context.append(")");
-		} else if (ast.operator.equals("~=")) {
-			context = ast.right.traverse(this, context.append("("));
-			context = ast.left.traverse(this, context.append(".test("));
-			return context.append(")");
-		} else if (ast.operator.equals("!~")) {
-			context = ast.right.traverse(this, context.append("(!("));
-			context = ast.left.traverse(this, context.append(".test("));
-			return context.append("))");
-		} else if (ast.operator.equals("~")) { 
-			context = ast.right.traverse(this, context.append("("));
-			context = ast.left.traverse(this, context.append(".exec("));
-			return context.append(")");
-		} else if (ast.operator.equals("/\\")) {
-			ast.left.traverse(this, context.append("this.$intersect("));
-			ast.right.traverse(this, context.append(","));
-			return context.append(")");
-		} else if (ast.operator.equals("\\/")) { 
-			ast.left.traverse(this, context.append("this.$union("));
-			ast.right.traverse(this, context.append(","));
-			return context.append(")");
+		if (JSONQLBinaryExpression.BINARY_OPERATORS.contains(ast.operator)) {
+			if (ast.operator.equals("~=")) {
+				context = ast.right.traverse(this, context.append("("));
+				context = ast.left.traverse(this, context.append(".test("));
+				return context.append(")");
+			} else if (ast.operator.equals("!~")) {
+				context = ast.right.traverse(this, context.append("(!("));
+				context = ast.left.traverse(this, context.append(".test("));
+				return context.append("))");
+			} else if (ast.operator.equals("~")) { 
+				context = ast.right.traverse(this, context.append("("));
+				context = ast.left.traverse(this, context.append(".exec("));
+				return context.append(")");
+			} else if (ast.operator.equals("/\\")) {
+				ast.left.traverse(this, context.append("this.$intersect("));
+				ast.right.traverse(this, context.append(","));
+				return context.append(")");
+			} else if (ast.operator.equals("\\/")) { 
+				ast.left.traverse(this, context.append("this.$union("));
+				ast.right.traverse(this, context.append(","));
+				return context.append(")");
+			} else { // A JavaScript operator.
+				ast.left.traverse(this, context.append("("));
+				ast.right.traverse(this, context.append(ast.operator));
+				return context.append(")");
+			}
 		} else {
 			throw new RuntimeException("Operator `"+ ast.operator +"` is not supported!");
 		}
@@ -86,6 +91,7 @@ public class JSONQL_JSCompiler implements JSONQLVisitor<StringBuilder, StringBui
 	}
 
 	@Override public StringBuilder visit(JSONQLIdentifier ast, StringBuilder context) {
+		//FIXME Check if the identifier is a reserved word for JavaScript.
 		return context.append(ast.name);
 	}
 
@@ -111,11 +117,11 @@ public class JSONQL_JSCompiler implements JSONQLVisitor<StringBuilder, StringBui
 	@Override public StringBuilder visit(JSONQLObjectExpression ast, StringBuilder context) {
 		//FIXME This may not generate valid JS, because of the expressions for the keys.
 		List<JSONQLProperty> properties = JSONQLObjectExpression.propertyList(ast.properties);
-		int i = 0;
-		for (JSONQLProperty property : properties) {
-			context.append(i == 0 ? "{" : ",");
-			property.traverse(this, context);
-		}
+		IntStream.range(0, properties.size()).forEach(i -> {
+				JSONQLProperty property = properties.get(i);
+				context.append(i == 0 ? "{" : ",");
+				property.traverse(this, context);
+			});
 		return context.append("}");
 	}
 
@@ -130,12 +136,27 @@ public class JSONQL_JSCompiler implements JSONQLVisitor<StringBuilder, StringBui
 	}
 
 	@Override public StringBuilder visit(JSONQLUnaryExpression ast, StringBuilder context) {
-		if ("! + - ".indexOf(ast.operator +" ") < 0) { // A JavaScript operator.
+		if (JSONQLUnaryExpression.UNARY_OPERATORS.contains(ast.operator)) {
+			// A JavaScript operator.
 			context.append("(").append(ast.operator).append(" ");
 			ast.argument.traverse(this, context);
 			return context.append(")");
 		} else {
 			throw new RuntimeException("Operator `"+ ast.operator +"` is not supported!");
 		}
+	}
+	
+	/** Main method for testing purposes.
+	 */
+	public static void main(String[] args) {
+		JSONQL_JSCompiler compiler = new JSONQL_JSCompiler();
+		JSONQLExpression ast = new JSONQLRoot();
+		/*
+		 JSONQLExpression ast = new JSONQLBinaryExpression("+",
+				new JSONQLLiteral(77),
+				new JSONQLLiteral(33)
+			);
+		*/
+		System.out.println(compiler.toJS(ast));
 	}
 }

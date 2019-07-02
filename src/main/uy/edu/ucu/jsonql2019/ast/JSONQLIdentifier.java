2a0c4918c962ca30f08f0783305d94949427da7e
LeonardoVal <leonardo.val@creatartis.com>
2019-07-02 19:46:18 -0300

    Added a static set of Javascript reserved words.
    + Meant to be used by the compiler to check for invalid identifiers.

--- src/main/uy/edu/ucu/jsonql2019/ast/JSONQLIdentifier.java
+++ src/main/uy/edu/ucu/jsonql2019/ast/JSONQLIdentifier.java
@@ -2,9 +2,17 @@ package uy.edu.ucu.jsonql2019.ast;
 
 import uy.edu.ucu.jsonql2019.JSONQLVisitor;
 
+import java.util.*;
+
 /** Class for AST nodes for identifiers.
  */
 public class JSONQLIdentifier extends JSONQLExpression {
+	public static Set<String> JS_RESERVED_WORDS = new HashSet<>(Arrays.asList(
+			"break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else",
+			"export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "return",
+			"super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"
+	));
+
 	public final String name;
 	
 	public JSONQLIdentifier(String name) {

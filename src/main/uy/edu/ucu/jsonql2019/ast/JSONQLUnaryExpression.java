f05eccf3a17a030bd39679c28fce0e4c4dd553c0
LeonardoVal <leonardo.val@creatartis.com>
2019-07-02 17:56:30 -0300

    Added helper methods for building test cases.

--- src/main/uy/edu/ucu/jsonql2019/ast/JSONQLUnaryExpression.java
+++ src/main/uy/edu/ucu/jsonql2019/ast/JSONQLUnaryExpression.java
@@ -7,10 +7,12 @@ import uy.edu.ucu.jsonql2019.JSONQLVisitor;
 /** Class for AST nodes for the following operators in JSONQL: `!`, `+`, `-`. 
  */
 public class JSONQLUnaryExpression extends JSONQLExpression {
+	/** Set of supported unary operators.
+	 */
 	public static final Set<String> UNARY_OPERATORS = new HashSet<String>(
 			Arrays.asList("!", "+", "-")
 		);
-	
+
 	public final String operator;
 	public final JSONQLExpression argument;
 	
@@ -29,4 +31,23 @@ public class JSONQLUnaryExpression extends JSONQLExpression {
 	@Override public <R, C> R traverse(JSONQLVisitor<R, C> visitor, C context) {
 		return visitor.visit(this, context);
 	}
+
+	/** Builder of expressions with negations of literals. Useful for making tests.
+	 */
+	public static JSONQLUnaryExpression not(Object value) {
+		return new JSONQLUnaryExpression("!", new JSONQLLiteral(value));
+	}
+
+	/** Builder of expressions with the unary `+` operator applied to a literal. Useful for making tests.
+	 */
+	public static JSONQLUnaryExpression unaryPlus(Object value) {
+		return new JSONQLUnaryExpression("+", new JSONQLLiteral(value));
+	}
+
+	/** Builder of expressions with the unary `-` operator applied to a literal. Useful for making tests.
+	 */
+	public static JSONQLUnaryExpression unaryMinus(Object value) {
+		return new JSONQLUnaryExpression("-", new JSONQLLiteral(value));
+	}
+
 }

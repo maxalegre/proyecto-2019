63000e8ab358cacd3ed8ab297bfb968ad7115dab
Jose <rodriguezshaw@gmail.com>
2019-07-04 20:05:22 -0300

    driver

---
+++ src/main/main/Driver.java
@@ -0,0 +1,27 @@
+package main;
+import uy.edu.ucu.jsonql2019.parser.*;
+import java.io.FileReader;
+import java.util.Scanner;
+import java_cup.runtime.*;
+
+public class Driver {
+
+	public static void parseFile(String file) throws Exception
+	{
+		JSONQLParser parser = new JSONQLParser(new JSONQLLexer(new FileReader(file)));
+		Symbol result = parser.parse();
+        System.out.println(result.sym + ": " + result.value);
+	}
+	public static void main(String[] args) {
+		try {
+			
+			 parseFile("input.txt");
+			 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
